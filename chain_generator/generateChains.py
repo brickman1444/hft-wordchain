b43d587d9dcc2a266d17f3bc6445feb67e58600e
Zac <brickman1444@gmail.com>
2015-07-31 23:24:58 -0500

    Very naive method of generating chains. Runs quickly and generated 36 chains

--- chain_generator/generateChains.py
+++ chain_generator/generateChains.py
@@ -1,14 +1,48 @@
 
 import json
 
-def getListsFromDictionary(pairDictionary):
+def appendWordToChain(partialChain, pairDictionary):
+
+	if partialChain is None:
+		return None
+
+	lastWord = partialChain[-1]
+
+	if lastWord in pairDictionary:
+		nextWord = pairDictionary[lastWord][0]
+	else:
+		print("end of chain")
+		return None
+
+	if nextWord in lastWord:
+		print("Found loop")
+		return None
+	else:
+		partialChain.append(nextWord)
+		return partialChain
+
+
+def naiveMethod(pairDictionary):
 
 	retList = []
 
-	for firstWord, secondWord in pairDictionary.items():
-		retList.append( [firstWord,secondWord[0]] )
+	for firstWord, secondWordList in pairDictionary.items():
+
+		chain = [firstWord,secondWordList[0]]
+
+		chain = appendWordToChain(chain,pairDictionary)
+		chain = appendWordToChain(chain,pairDictionary)
+		chain = appendWordToChain(chain,pairDictionary)
+		chain = appendWordToChain(chain,pairDictionary)
+
+		if chain is not None:
+			retList.append( chain )
 
 	return retList
+
+def getListsFromDictionary(pairDictionary):
+
+	return naiveMethod(pairDictionary)
 
 # main
 
@@ -19,6 +53,8 @@ file = open( wordPairFileName, 'r' )
 pairRoot = json.load( file )
 
 lists = getListsFromDictionary( pairRoot["word pairs"] )
+
+print("Generated %d lists" % len(lists))
 
 listsRoot = {}
 listsRoot["wordLists"] = lists
