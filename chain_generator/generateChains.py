52c6b3263a3a8fd5e26680460e168de076f49670
Zac <brickman1444@gmail.com>
2015-08-01 00:06:38 -0500

    Full recursive naive method of generating chains. Generates 427 chains!

--- chain_generator/generateChains.py
+++ chain_generator/generateChains.py
@@ -1,42 +1,59 @@
 
 import json
+import copy
 
-def appendWordToChain(partialChain, pairDictionary):
+# constants
+
+wordPairFileName = "wordPairs.json"
+wordChainFileName = "wordLists.json"
+
+chainLength = 6
+
+def appendWordsToChain(partialChain, retList, shortList, pairDictionary):
 
 	if partialChain is None:
-		return None
+		return
 
 	lastWord = partialChain[-1]
 
-	if lastWord in pairDictionary:
-		nextWord = pairDictionary[lastWord][0]
-	else:
-		print("end of chain")
-		return None
+	if lastWord not in pairDictionary:
+		shortList.append(partialChain)
+		return
 
-	if nextWord in lastWord:
-		print("Found loop")
-		return None
-	else:
-		partialChain.append(nextWord)
-		return partialChain
+	nextWords = pairDictionary[lastWord]
 
+	for nextWord in nextWords:
+		if nextWord in partialChain:
+			shortList.append(partialChain)
+			return
+		else:
+			newChain = copy.copy(partialChain)
+			newChain.append(nextWord)
+	
+		if len(newChain) < chainLength:
+			appendWordsToChain(newChain,retList,shortList,pairDictionary)
+		else:
+			retList.append( newChain )
 
 def naiveMethod(pairDictionary):
 
 	retList = []
+	shortList = [] # to hold lists that are too short
 
-	for firstWord, secondWordList in pairDictionary.items():
+	for firstWord in pairDictionary:
 
-		chain = [firstWord,secondWordList[0]]
+		chain = [firstWord]
 
-		chain = appendWordToChain(chain,pairDictionary)
-		chain = appendWordToChain(chain,pairDictionary)
-		chain = appendWordToChain(chain,pairDictionary)
-		chain = appendWordToChain(chain,pairDictionary)
+		chain = appendWordsToChain(chain,retList,shortList,pairDictionary)
 
-		if chain is not None:
-			retList.append( chain )
+	print("Short List:")
+	shortCount = 0
+	for list in shortList:
+		if len(list) == (chainLength - 1):
+			print(list)
+			shortCount += 1
+
+	print("Just short count: %d" % shortCount)
 
 	return retList
 
@@ -44,15 +61,39 @@ def getListsFromDictionary(pairDictionary):
 
 	return naiveMethod(pairDictionary)
 
-# main
+def getReverseDictionary(pairDictionary):
 
-wordPairFileName = "wordPairs.json"
-wordChainFileName = "wordLists.json"
+	retDict = {}
+
+	# go through the whole list
+	for firstWord, secondWords in pairDictionary.items():
+		# for each second word
+		for secondWord in secondWords:
+
+			# only process a second word once
+			if secondWord in retDict:
+				continue
+			
+			retDict[secondWord] = []
+			# go through the whole list again and collect all the first words for it
+			for firstWord2, secondWords2 in pairDictionary.items():
+				if secondWord in secondWords2:
+
+					if firstWord2 not in retDict[secondWord]:
+						#print("%s %s" % (secondWord, firstWord2))
+						retDict[secondWord].append(firstWord2)
+
+	return retDict
+
+# main
 
 file = open( wordPairFileName, 'r' )
 pairRoot = json.load( file )
 
-lists = getListsFromDictionary( pairRoot["word pairs"] )
+pairDictionary = pairRoot["word pairs"]
+reverseDictionary = getReverseDictionary( pairDictionary )
+
+lists = getListsFromDictionary( pairDictionary )
 
 print("Generated %d lists" % len(lists))
 
