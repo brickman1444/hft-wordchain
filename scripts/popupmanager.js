e77ccf34b7be1aabab26aae658439a77ffe4d1e8
Zac <brickman1444@gmail.com>
2015-08-02 22:42:35 -0500

    Skeleton of popup system

---
+++ scripts/popupmanager.js
@@ -0,0 +1,28 @@
+
+"use strict";
+
+function $(id) {
+  return document.getElementById(id);
+}
+
+define( [
+        ],
+       function (
+                 ) {
+
+
+  var PopUpManager = function (services) {
+    this.services = services;
+      
+      alert("alert")
+      
+  };
+    
+  PopUpManager.prototype.func = function()
+  {
+     
+  }
+
+  return PopUpManager;
+});
+
