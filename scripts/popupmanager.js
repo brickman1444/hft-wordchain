93e1355863d30c21c0360758c493ed14b257ba4e
Zac <brickman1444@gmail.com>
2015-08-02 23:23:55 -0500

    Got a basic popup and timing proof of concept working

--- scripts/popupmanager.js
+++ scripts/popupmanager.js
@@ -14,9 +14,24 @@ define( [
   var PopUpManager = function (services) {
     this.services = services;
       
-      alert("alert")
+      this.overlay = $("overlay");
+      this.line1 = $("overlay-line1");
+      this.line2 = $("overlay-line2");
       
+      this.HidePopup();
+      
+      setTimeout( this.ShowPopUp, 3000 );
   };
+    
+  PopUpManager.prototype.ShowPopUp = function()
+  {
+    this.overlay.style.visibility = 'visible';  
+  }
+  
+  PopUpManager.prototype.HidePopup = function()
+  {
+    this.overlay.style.visibility = 'hidden';
+  }
     
   PopUpManager.prototype.func = function()
   {
