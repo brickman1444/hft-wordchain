12bde1ef7041a325bc06494bee3e597c8dc05d39
Zac <brickman1444@gmail.com>
2015-08-29 00:49:32 -0500

    skeleton of timer

---
+++ scripts/timerManager.js
@@ -0,0 +1,22 @@
+
+"use strict";
+
+define( [],
+       function (
+                 ) {
+    
+  var turnTime = 30;
+    
+  var TimerManager = function (services) {
+    this.services = services;
+
+  };
+    
+  TimerManager.prototype.func = function()
+  {
+      
+  }
+
+  return TimerManager;
+});
+
